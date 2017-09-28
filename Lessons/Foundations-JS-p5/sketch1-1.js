5af3595b5aa5ac1e6b27a4ef1b7b65574a5515d7
Malcolm Gin <malcolm.gin@gmail.com>
2017-09-28 13:51:02 -0700

    Color fix.

--- Lessons/Foundations-JS-p5/sketch1-1.js
+++ Lessons/Foundations-JS-p5/sketch1-1.js
@@ -3,7 +3,7 @@ function setup() {
   // The Coding Train has a line drawing of a rectangle. Assume p5 defaults now to filled with black.
   createCanvas(200, 200);
   // set fill to white
-  fill(0)
+  fill(255)
 }
 
 function draw() {
