c43c5532fc8b84be949346168d99dd1a4f5c1663
Malcolm Gin <malcolm.gin@gmail.com>
2017-09-28 13:50:20 -0700

    Getting used to how Atom works.

--- Lessons/Foundations-JS-p5/sketch1-1.js
+++ Lessons/Foundations-JS-p5/sketch1-1.js
@@ -1,7 +1,12 @@
 function setup() {
-
+  // setup() gets called once
+  // The Coding Train has a line drawing of a rectangle. Assume p5 defaults now to filled with black.
+  createCanvas(200, 200);
+  // set fill to white
+  fill(0)
 }
 
 function draw() {
-
+  // draw() gets called on a cycle (so it can keep rendering)
+  rect(50, 50, 20, 20)
 }
