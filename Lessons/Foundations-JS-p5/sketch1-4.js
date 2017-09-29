4e3b98616a5797645e635634d4f1cbf1174aa28c
Malcolm Gin <malcolm.gin@gmail.com>
2017-09-28 17:58:52 -0700

    Stuff added during lecture.

--- Lessons/Foundations-JS-p5/sketch1-4.js
+++ Lessons/Foundations-JS-p5/sketch1-4.js
@@ -9,16 +9,23 @@ function setup() {
 
 function draw() {
   // draw() gets called on a cycle (so it can keep rendering).
-  background(255);
+  // white
+  //background(255);
+  // grey
+  // Also: background(150);
+  background(150, 150, 150);
   ellipseMode(CENTER);
   rectMode(CENTER);
 
   // Body
+  // Also can use a 4th argument with fill (transparency/alpha)
   fill(255,0,0);
   rect(240, 145, 20, 100);
 
   // Head
-  fill(0, 0, 255);
+  // Also can use a 4th argument with fill (transparency/alpha)
+  // fill(0, 0, 255);
+  fill(0, 0, 255, 50);
   ellipse(240, 115, 60, 60);
 
   // Eyes
