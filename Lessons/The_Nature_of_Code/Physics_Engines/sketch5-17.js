aa6bc930c6b13b7cc2167ad532a44a090e9f1e50
Malcolm Gin <malcolm.gin@gmail.com>
2017-09-29 07:11:31 -0700

    Initial p5 only start point.

--- Lessons/The_Nature_of_Code/Physics_Engines/sketch5-17.js
+++ Lessons/The_Nature_of_Code/Physics_Engines/sketch5-17.js
@@ -1,7 +1,9 @@
 function setup() {
-
+  // setup() gets called once
+  createCanvas(400, 400);
 }
 
 function draw() {
-
+  // draw() gets called on a cycle (so it can keep rendering).
+  background(51);
 }
