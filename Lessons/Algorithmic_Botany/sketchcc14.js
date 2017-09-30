dc1df4e6946199b7150a740939fc7f00843e3d55
Malcolm Gin <malcolm.gin@gmail.com>
2017-09-30 12:33:02 -0700

    Complete lesson (with strokeweights).

--- Lessons/Algorithmic_Botany/sketchcc14.js
+++ Lessons/Algorithmic_Botany/sketchcc14.js
@@ -9,6 +9,16 @@ var Engine = Matter.Engine,
 var engine;
 var world;
 
+// Branch/Recursion related variables
+var shortenFactor = 0.67;
+var shortestBranch = 4; // pixels
+var rotAngle;
+var toplen;
+var maxLevels; // of recursion, for this canvas size
+
+// Controls
+var slider;
+
 function setup() {
   // setup() gets called once
   createCanvas(windowWidth - 100, windowHeight - 100);
@@ -20,24 +30,60 @@ function setup() {
   world = engine.world;
   // make sure the world is running
   Engine.run(engine);
-  // make a thing for boxes to bounce off of
-  // note option to make this body static, so it's not affected
-  // by gravity
-  var options = {
-    isStatic: true
-  }
-  // height and width must be global variables
-  ground = Bodies.rectangle(width/2, height - 50, width, 100, options);
-  World.add(world, ground);
+
+  toplen = height/6;
+  maxLevels = findlevels(toplen);
+  rotAngle = PI/4;
+
+  slider = createSlider(0, TWO_PI, rotAngle, 0.01);
 }
 
 function draw() {
   // draw() gets called on a cycle (so it can keep rendering).
   background(51);
 
-  // render the ground
-  fill(120);
-  noStroke();
-  rectMode(CENTER);
-  rect(ground.position.x, ground.position.y, width, 100);
+  // get slider value for rotAngle
+  rotAngle = slider.value();
+
+  // draw trunk
+  stroke(255);
+  strokeWeight(4);
+  translate(width/2, height);
+  branch(toplen, maxLevels);
+}
+
+function branch(len, weight) {
+  // My first impulse was to define a temporary variable for weight
+  // forgetting, of course, that recursion takes care of this for you
+  // as it drills down the execution path/flow.
+  strokeWeight(weight);
+  line(0, 0, 0, -len);
+  translate(0, -len);
+  if (len > shortestBranch) {
+    // push() saves the current transformation
+    push();
+    rotate(rotAngle);
+    branch(len * shortenFactor, weight - 1);
+    // pop() restores the transformation
+    pop();
+    // save again
+    push();
+    rotate(-rotAngle);
+    branch(len * shortenFactor, weight - 1);
+    // restore again
+    pop();
+  }
+  // line(0, 0, 0, -len*0.67);
+}
+
+function findlevels(len) {
+  // Interrogate canvas dimensions, dynamically, to figure out
+  // how many recursive levels to expect
+  // returns integer
+  var levels = 0;
+  while (len > shortestBranch) {
+    len = len * shortenFactor;
+    levels = levels + 1;
+  }
+  return levels;
 }
