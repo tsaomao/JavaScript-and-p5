3c3a907d1f68a4b52264ce1b10870c2c6824ec1b
Malcolm Gin <malcolm.gin@gmail.com>
2017-09-30 15:24:16 -0700

    Setup for next lecture.

---
+++ Lessons/Algorithmic_Botany/sketchcc15.js
@@ -0,0 +1,60 @@
+// Template includes basic matter js cleanup
+// matter.js module aliases
+var Engine = Matter.Engine,
+  // We're gonna do our own rendering in p5.js
+  // Render = Matter.Render,
+  World = Matter.World,
+  Bodies = Matter.Bodies;
+
+var engine;
+var world;
+
+// Canvas positioning
+var cnv;
+
+function setup() {
+  // setup() gets called once
+  cnv = createCanvas(windowWidth - 100, windowHeight - 200);
+  centerCanvas();
+
+  // make shapes get filled with white instead of default black
+  fill(255);
+  // create an engine
+  engine = Engine.create();
+  // create world var to keep track of the world
+  world = engine.world;
+  // make sure the world is running
+  Engine.run(engine);
+}
+
+function draw() {
+  // draw() gets called on a cycle (so it can keep rendering).
+  background(51);
+
+  // get slider value for rotAngle
+  rotAngle = slider.value();
+  jitterAngle = jitterSlider.value();
+
+  // draw trunk
+  stroke(255);
+  strokeWeight(4);
+  translate(width/2, height);
+  branch(toplen, maxLevels);
+}
+
+function resizeCanvas_m() {
+  var width = windowWidth - 100;
+  var height = windowHeight - 200;
+  resizeCanvas(width, height);
+}
+
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
+
+function windowResized() {
+  centerCanvas();
+  resizeCanvas_m();
+}
